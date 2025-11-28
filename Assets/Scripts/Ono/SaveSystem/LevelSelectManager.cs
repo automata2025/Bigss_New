13c8779bae0f4097d9c494b47af470cdd3ea6017
automata2025 <ono.surasingsarit@gmail.com>
2025-11-27 23:08:20 -0800

    Final Fix

--- Assets/Scripts/Ono/SaveSystem/LevelSelectManager.cs
+++ Assets/Scripts/Ono/SaveSystem/LevelSelectManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelSelectManager : MonoBehaviour
 {
@@ -31,5 +32,34 @@ public class LevelSelectManager : MonoBehaviour
 
             panel.Apply(data, unlocked);
         }
+    }
+
+    public void OnNewGameClicked()
+    {
+        Debug.Log("[LevelSelectManager] New Game clicked.");
+        SaveSystem.ClearSave();
+
+        SaveSystem.Load();
+
+        var save = SaveSystem.CurrentSave;
+        if (save == null)
+        {
+            Debug.LogError("[LevelSelectManager] After ClearSave, CurrentSave is null!");
+            return;
+        }
+
+        Debug.Log($"[LevelSelectManager] After ClearSave: lastUnlockedLevelIndex = {save.lastUnlockedLevelIndex}");
+
+        if (save.levels != null)
+        {
+            foreach (var lvl in save.levels)
+            {
+                Debug.Log(
+                    $"[LevelSelectManager] Level {lvl.levelId} (index {lvl.levelIndex}) " +
+                    $"cleared={lvl.cleared}, bestTime={lvl.bestTime}, bestStars={lvl.bestStars}"
+                );
+            }
+        }
+        RefreshUI();
     }
 }
